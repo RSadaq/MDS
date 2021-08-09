f9dcf5a8a8c015159bb73cc44f63db9dcbdce39d
rsadaq <rsadaq@gmail.com>
2021-08-09 18:27:08 +0100

    icons added to tab-bar, tab-bar colours amended

--- screens/Navigator.js
+++ screens/Navigator.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialIcons, FontAwesome, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './HomeScreen';
@@ -22,15 +22,40 @@ export default function Navigator() {
         <NavigationContainer>
         <Tab.Navigator
           screenOptions={() => ({
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray',
+            tabBarActiveTintColor: 'red',
+            tabBarInactiveTintColor: 'black',
           })}
         >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Blogs" component={BlogsScreen} />
-          <Tab.Screen name="Podcasts" component={Podcasts} />
-          <Tab.Screen name="FAQs" component={FAQsScreen} />
-          <Tab.Screen name="Contact" component={ContactScreen} />
+          <Tab.Screen name="Home" component={HomeScreen} options={{
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ color, size }) => (
+        <Ionicons name="home" color={color} size={size} />
+      ),
+    }}/>
+          <Tab.Screen name="Blogs" component={BlogsScreen} options={{
+            tabBarLabel: 'Blogs',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="pencil-square-o" color={color} size={size}/>
+            ),
+          }}/>
+          <Tab.Screen name="Podcasts" component={Podcasts} options={{
+            tabBarLabel: 'Podcasts',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="headphones-alt" color={color} size={size}/>
+            ),
+          }}/>
+          <Tab.Screen name="FAQs" component={FAQsScreen} options={{
+            tabBarLabel: 'FAQs',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="frequently-asked-questions" color={color} size={size}/>
+            ),
+          }}/>
+          <Tab.Screen name="Contact Us" component={ContactScreen} options={{
+            tabBarLabel: 'Contact',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="contact-mail" color={color} size={size}/>
+            ),
+          }}/>
         </Tab.Navigator>
       </NavigationContainer>
     );
