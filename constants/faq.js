1476c9ae220e0e18aa0b110e41028d455510ad8f
MrJoedf <joefoster0011@gmail.com>
2021-08-11 20:41:43 -0500

    FAQ update

---
+++ constants/faq.js
@@ -0,0 +1,28 @@
+
+
+export const FAQ = [{
+    question: 'My organization wants to partner with MDS. How can I do that?',
+    answer: 'Please email us at info@mind-designsports.org for any requests or questions about partnerships, speaking engagements, guest blog posts, podcast participants, interviews, and media inquiries.'
+}, 
+
+{
+    question: 'How can I get involved in MDS’s mission?',
+    answer: 'Reshare our posts/videos on social media, write a guest blog post, and spread the word about our organization. Share the organization and our website with your friends as well. If you are a high school or college level athlete, become a mentor and volunteer :)'
+},
+
+{
+    question: 'What if I have a specific topic that I want to be addressed?',
+    answer: 'If there’s a specific blog/podcast topic you’d like to learn about, let us know by filling out this quick form!'
+},
+
+{
+    question: 'Why should I utilize the content on MDS versus a professional sports psychology website?',
+    answer: 'We are the only sports psychology organization governed by students that play sports so we have a perspective that is directly applicable to younger athletes. The writing in the blogs and the questions asked in the podcasts will reflect that and allow younger athletes to take away the most applicable information to help them perform their best . The majority of other sports psychology websites use extremely sophisticated language without taking into account how younger athletes will utilize the information and some are also targeted towards parents for their kids rather than directly to the kids.'
+
+}, 
+
+{
+    question: 'I want more one-on-one guidance. What should I do?',
+    answer: 'You can email us at info@mind-designsports.org if you have specific questions and we will respond as soon as possible. However, we do not provide paid one on one consulting.'
+}
+];
--- screens/FAQsScreen.js
+++ screens/FAQsScreen.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
-import { ImageBackground, Text, View } from 'react-native';
+import { Dimensions, ImageBackground, ScrollView, Text, View } from 'react-native';
 import {Collapse,CollapseHeader, CollapseBody, AccordionList} from 'accordion-collapse-react-native';
 import { backgrounds } from '../constants';
 
+import Accordion from '../constants/Accordion'
 export default function FAQsScreen() {
     return (  
-    <ImageBackground source={backgrounds.soccer} resizeMode='cover' style={{ flex:1, justifyContent:'center' }} imageStyle={{ top:-80 }}>
-              
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>FAQs!</Text>
-                
+    
+        <ScrollView>  
+    
+       
+  <Accordion/>
 
-            </View>
-    </ImageBackground>
+           
+            </ScrollView>     
+
      
     );
   }
