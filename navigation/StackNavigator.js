480b1fac25e78aa84859401b0dca139ef76d3977
rsadaq <rsadaq@gmail.com>
2021-08-16 16:30:57 +0100

    Clickable cards added to BlogsScreen

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import BlogsScreen from "../screens/BlogsScreen";
 import Tennis from "../blogs/Tennis";
+import Running from "../blogs/Running";
 
 const Stack = createStackNavigator();
 
@@ -19,6 +20,7 @@ const MainStackNavigator = () => {
     <Stack.Navigator screenOptions={screenOptionStyle}>
       <Stack.Screen name="BlogsStack" component={BlogsScreen}  options = {{headerShown: false}}/>
       <Stack.Screen name="Tennis" component={Tennis} />
+      <Stack.Screen name="Running" component={Running} />
     </Stack.Navigator>
   );
 }
--- screens/BlogsScreen.js
+++ screens/BlogsScreen.js
@@ -1,14 +1,45 @@
 import React, { useState } from 'react';
-import { Button, Stylesheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet} from 'react-native';
+import { Card, Title } from 'react-native-paper';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default function BlogsScreen( { navigation }) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Blogs!</Text>
-        <Button
-        title="Go to Tennis blog!"
-        onPress={() => navigation.navigate("Tennis")}
-      />
-      </View>
-    );
+export default function BlogsScreen({ navigation }) {
+  return (
+
+    <View style={styles.pageStyling}>
+      <TouchableOpacity onPress={() => navigation.navigate("Tennis")}>
+        <Card containerStyle={{ flex: 2, marginLeft: 5, marginRight: 5, marginTop: 20, height: 230, width: 170, borderRadius: 70}}>
+          <Card.Cover source={require('../assets/images/blog-images/tennis-player.jpeg')} style={{ position: 'relative', height: 180, width: 170 }} />
+          <Card.Content style={{ width: 170, backgroundColor: 'firebrick' }}>
+            <Title style={styles.titleText}>Becoming a Champion of the Court (but without the racket)</Title>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate("Running")}>
+        <Card containerStyle={{ flex: 2, marginLeft: 5, marginRight: 5, marginTop: 20, height: 230, width: 170}}>
+          <Card.Cover source={require('../assets/images/blog-images/running.jpeg')} style={{ position: 'relative', height: 180, width: 170 }} />
+          <Card.Content style={{ width: 170, height: 110,  backgroundColor: 'firebrick' }}>
+            <Title style={styles.titleText}>Need to break out of your slump?</Title>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    </View>
+
+  );
+};
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 12,
+    fontStyle: 'italic',
+    fontWeight: "bold", 
+    color: 'white'
+  },
+
+  pageStyling: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10
   }
+})
